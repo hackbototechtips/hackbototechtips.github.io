a3aeeaeb8ec0927dd833b25786e2c970b94c80d9
Hackboto Tech Tips <hackbototechtips@gmail.com>
2024-05-28 19:00:09 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+//This is used to activate the hamburger menu.
+function navHamburger() {
+    var x = document.getElementById("Nav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+  }
+}
+// Fixes the aside bar
+function updateAsideHeight() {
+  const headerHeight = document.querySelector('header').offsetHeight;
+  const navHeight = document.querySelector('nav').offsetHeight;
+  const aside = document.querySelector('aside');
+  const totalHeight = headerHeight + navHeight;
+  const availableHeight = window.innerHeight - totalHeight - 55; // 55px for padding
+  aside.style.top = `${totalHeight}px`;
+  aside.style.height = `${availableHeight}px`;
+}
+
+window.addEventListener('resize', updateAsideHeight);
+window.addEventListener('load', updateAsideHeight);
